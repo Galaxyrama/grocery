85edbaf9c3f707d302417a09b55b3857664829ed
Joshua Aldueza <alduezajoshua@gmail.com>
2025-02-17 09:56:17 +0800

    second commit

--- src/components/GroceryComponent.jsx
+++ src/components/GroceryComponent.jsx
@@ -1,29 +1,79 @@
-import React, { useState } from 'react'
-import {v4 as uuid} from "uuid"
+import React, { useRef, useState } from "react";
+import { v4 as uuid } from "uuid";
+import GroceryItemComponent from "./GroceryItemComponent";
 
 const GroceryComponent = () => {
-    const [item, setItem] = useState("");
-    const [groceryItems, setGroceryItems] = useState([]);
+  const inputRef = useRef();
+  const [item, setItem] = useState("");
+  const [groceryItems, setGroceryItems] = useState([]);
+  const [errors, setErrors] = useState("");
 
-    const handleAddItem = () => {
-        setGroceryItems([...groceryItems, {id: uuid(), name: item}]);
+  const handleAddItem = () => {
+    if (item) {
+      setGroceryItems([...groceryItems, { id: uuid(), name: item }]);
+      setItem("");
+      setErrors("");
+    } else {
+      setErrors("Grocery item cannot be empty.");
+      inputRef.current.focus();
     }
+  };
 
-    return (
-        <div className='grocery-buddy'>
-            <h1>Grocery Buddy</h1>
-            <div className='input-section'>
-                <div className='input-container'>
-                    <input type="text" onChange={(event) => setItem(event.target.value)} 
-                        placeholder='Enter an item...' value={item}/>
-                    <button className="btn-add" onClick={handleAddItem}>Add Item</button>
-                </div>
-            </div>
-            <ul className='grocery-list'>
-                {groceryItems.map((item))}
-            </ul>
+  const handleEditItem = (id, newItem) => {
+    const updatedGroceryItems = groceryItems.map((item) => {
+      if (item.id === id) {
+        return { ...item, name: newItem };
+      }
+
+      return item;
+    });
+    setGroceryItems(updatedGroceryItems);
+  };
+
+  const handleDeleteItem = (removeId) => {
+    const filteredItems = groceryItems.filter((item) => item.id !== removeId);
+    setGroceryItems(filteredItems);
+  };
+
+  const handleClearItems = () => {
+    setGroceryItems([]);
+  };
+
+  return (
+    <div className="grocery-buddy">
+      <h1>Grocery Buddy</h1>
+      <div className="input-section">
+        <div className="input-container">
+          <input
+            ref={inputRef}
+            type="text"
+            placeholder="Enter an item..."
+            value={item}
+            onChange={(event) => setItem(event.target.value)}
+          />
+          <button onClick={handleAddItem} className="btn-add">
+            Add Item
+          </button>
         </div>
-    )
-}
+        <div>{errors ? <p className="errors">{errors}</p> : null}</div>
+      </div>
+      <ul className="grocery-list">
+        {groceryItems.map((item) => (
+          <GroceryItemComponent
+            key={item.id}
+            item={item}
+            handleEditItem={handleEditItem}
+            handleDeleteItem={handleDeleteItem}
+          />
+        ))}
+      </ul>
+      {groceryItems.length > 0 ? (
+        <button onClick={handleClearItems} className="btn-clear">
+          Clear Grocery Items{" "}
+        </button>
+      ) : null}
+    </div>
+  );
+};
 
-export default GroceryComponent
+export default GroceryComponent;
--- src/components/GroceryItemComponent.jsx
+++ src/components/GroceryItemComponent.jsx
@@ -1,9 +1,53 @@
-import React from 'react'
+import React, { useState } from "react";
 
-const GroceryItemComponent = () => {
+const GroceryItemComponent = ({ item, handleEditItem, handleDeleteItem }) => {
+  const [isEditing, setIsEditing] = useState(false);
+  const [newItem, setNewItem] = useState(item.name);
+  const [errors, setErrors] = useState("");
+
+  const onEdit = () => {
+    if (newItem) {
+      handleEditItem(item.id, newItem);
+      setIsEditing(false);
+      setErrors("");
+    } else {
+      setErrors("Grocery item must not be empty.");
+    }
+  };
+
   return (
-    <div>GroceryItemComponent</div>
-  )
-}
+    <>
+      <li>
+        {isEditing ? (
+          <input
+            type="text"
+            value={newItem}
+            onChange={(event) => setNewItem(event.target.value)}
+          />
+        ) : (
+          <span>{item.name}</span>
+        )}
 
-export default GroceryItemComponent
+        <div>
+          <button
+            onClick={() => {
+              isEditing ? onEdit() : setIsEditing(true);
+            }}
+            className="btn-edit"
+          >
+            {isEditing ? "Save" : "Edit"}
+          </button>
+          <button
+            onClick={() => handleDeleteItem(item.id)}
+            className="btn-delete"
+          >
+            Delete
+          </button>
+        </div>
+      </li>
+      {errors ? <p className="errors">{errors}</p> : null}
+    </>
+  );
+};
+
+export default GroceryItemComponent;
